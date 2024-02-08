9d10da7b11f5eeb52f393615b16bc7257205c8d7
Cloves Neto <cvr.neto20@gmail.com>
2024-02-08 04:33:22 -0300

    nlw-aula-1

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,32 @@
-
+import Logo from './assets/logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
-  return <h1>Olá mundo</h1>
+  return(      
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={Logo} />
+      <form className='w-full'>
+        <input
+        type="text" 
+        placeholder='Busque as suas notas...'
+        className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500 '
+        />  
+      </form>
+      {/* Linha divisória de conteúdo */}
+      <div className='h-px bg-slate-700' />
+
+      {/* Area de cards de anotação */}
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+          {/*Cards de notas */}
+          <NewNoteCard></NewNoteCard>
+          <NoteCard></NoteCard>
+          <NoteCard></NoteCard>
+          <NoteCard></NoteCard>
+          <NoteCard></NoteCard>
+      </div>
+    </div>
+  )    
 }
 
 
